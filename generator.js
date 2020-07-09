0e4d3ddd38fb98fed363b135cb19da47686e2980
peelstnac <chfrn2002@gmail.com>
2020-07-09 15:12:16 -0400

    Stack added

--- generator.js
+++ generator.js
@@ -20,6 +20,18 @@ class Rectangle {
     this.width = width;
     this.height = height;
   }
+
+  intersect (rect) {
+    var thisCenter = new Point(this.anchor.x + this.width / 2, this.anchor.y + this.height / 2);
+    var thatCenter = new Point(rect.anchor.x + rect.width / 2, rect.anchor.y + rect.height / 2);
+    var threshold = [this.width / 2 + rect.width / 2, this.height / 2 + rect.height / 2];
+    if (Math.abs(thisCenter.x - thatCenter.x) < threshold[0] &&
+      Math.abs(thisCenter.y - thatCenter.y) < threshold[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 // Generate random point in a circle as described by [1]
 function circlePoint (radius) {
@@ -46,14 +58,41 @@ function genMap (limit, radius, roomConfig) {
     rectList.push(new Rectangle(point, width, height));
   }
   // Follow instructions of [2]
-  // Keep track of the rectangles in adjacency list
-  var inAdj = Array(limit).fill(1);
-  var deg = Array(limit).fill(0);
-  var adj = [...Array(limit)];
+  // Create an undirected graph to represent intersections
+  var deg = [...Array(limit)].map(e => [0, 0]);
+  for (let i = 0; i < limit; i++) {
+    deg[i][1] = i;
+  }
+  var adj = [...Array(limit)].map(e => []);
   // Map intersection relationships
   for (let i = 0; i < limit; i++) {
-    
+    for (let j = i + 1; j < limit; j++) {
+      let r1 = rectList[i];
+      let r2 = rectList[j];
+      if (r1.intersect(r2)) {
+        deg[i][0]++;
+        deg[j][0]++;
+        adj[i].push(j);
+        adj[j].push(i);
+      }
+    }
+  }
+  // While there are intersections
+  var stack = [];
+  while (true) {
+    // Sort in reverse
+    deg.sort();
+    deg.reverse();
+    if (deg[0][0] === -1) break;
+    // Push most intersected on stack
+    stack.unshift(deg[0][1]);
+    // Remove this rectangle
+    deg[0][0] = -1;
+    for (let x in adj[deg[0][1]]) {
+      if (deg[adj[deg[0][1]][x]][0] == -1) {
+        continue;
+      }
+      deg[adj[deg[0][1]][x]][0]--;
+    }
   }
 }
-
-module.exports = genMap();
